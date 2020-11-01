37b545e2d9640fc95eb186cdb25e6765c8c2b28e
mahir29 <mahirhardwar@gmail.com>
2020-11-01 10:52:40 +0530

    Updates

---
+++ precache-manifest.4d9678a24d9ecfb820241c53a5bcb6eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec42f378aace283b6cb15cea650cc80",
+    "url": "/dollaweb/index.html"
+  },
+  {
+    "revision": "45f1dee09a6849df9709",
+    "url": "/dollaweb/static/css/main.ebad143b.chunk.css"
+  },
+  {
+    "revision": "20bfbb556b7f260b29df",
+    "url": "/dollaweb/static/js/2.cad18f33.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/dollaweb/static/js/2.cad18f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f1dee09a6849df9709",
+    "url": "/dollaweb/static/js/main.cd3dd151.chunk.js"
+  },
+  {
+    "revision": "aa3de87c5a4d16833a5b",
+    "url": "/dollaweb/static/js/runtime-main.c0a0742f.js"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/dollaweb/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "4a122f73bc839268339ab652e3ec374d",
+    "url": "/dollaweb/static/media/svg-2.4a122f73.svg"
+  },
+  {
+    "revision": "99b1c6a663714094265da59a2ece9635",
+    "url": "/dollaweb/static/media/svg-3.99b1c6a6.svg"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/dollaweb/static/media/video.ed44dc5d.mp4"
+  }
+]);
